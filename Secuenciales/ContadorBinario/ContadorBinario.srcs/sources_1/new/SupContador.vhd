28466ef30fbebee6f96b8c353b8c4ed603e6478a
César Augusto Romero Molano <ceroma175@gmail.com>
2023-07-16 07:26:12 -0500

    sec2

---
+++ Secuenciales/ContadorBinario/ContadorBinario.srcs/sources_1/new/SupContador.vhd
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity SupContador is
+    Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           FoutCuenta : out STD_LOGIC_VECTOR (7 downto 0));
+end SupContador;
+
+architecture Behavioral of SupContador is
+
+component Divisor is
+    Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           FoutDivide : out STD_LOGIC);
+end component;
+
+component Contador is
+    Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           FoutCuenta : out STD_LOGIC_VECTOR (7 downto 0));
+end component;
+
+signal sg_clk: Std_logic;
+
+begin
+
+uo:Divisor Port map(clk=>clk,reset=>reset,Foutdivide=>sg_clk);
+u1:contador Port map(clk=>sg_clk,reset=>reset,Foutcuenta=>FoutCuenta);
+end Behavioral;
