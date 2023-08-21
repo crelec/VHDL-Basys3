c03d6c9c47b9bf427d97449e1d3b24907eaf33e3
César Augusto Romero Molano <ceroma175@gmail.com>
2023-08-21 06:42:33 -0500

    TesVisualDinamicoAuto

--- Secuenciales/MuxDisplayAuto/MuxDisplayAuto.runs/impl_1/SupMuxDisplayAuto.tcl
+++ Secuenciales/MuxDisplayAuto/MuxDisplayAuto.runs/impl_1/SupMuxDisplayAuto.tcl
@@ -115,8 +115,7 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
+set_msg_config -id {Common 17-41} -limit 10000000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Write Bitstream" START { ROLLUP_AUTO }
@@ -125,9 +124,7 @@ start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param checkpoint.writeSynthRtdsInDcp 1
   set_param chipscope.maxJobs 2
-  set_param synth.incrementalSynthesisCache C:/Users/cerom/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-14924-LenovoCesar/incrSyn
   set_param xicom.use_bs_reader 1
   set_param runs.launchOptions { -jobs 4  }
   open_checkpoint SupMuxDisplayAuto_routed.dcp
---
+++ Secuenciales/MuxDisplayAuto/MuxDisplayAuto.srcs/sim_1/new/Tes.vhd
@@ -0,0 +1,61 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Tes is
+--  Port ( );
+end Tes;
+
+architecture Behavioral of Tes is
+
+component SupMuxDisplayAuto is
+    Port ( DatoEntrada : in STD_LOGIC_VECTOR (13 downto 0);
+           clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           segmento : out STD_LOGIC_VECTOR (6 downto 0);
+           anodo : out STD_LOGIC_VECTOR (3 downto 0));
+end component;
+
+--Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+   signal DatoEntrada : STD_LOGIC_VECTOR (13 downto 0);
+   
+ 	--Outputs
+   signal segmento : STD_LOGIC_VECTOR (6 downto 0);
+   signal  anodo : STD_LOGIC_VECTOR (3 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+   
+begin
+
+-- Instantiate the Unit Under Test (UUT)
+   uut: SupMuxDisplayAuto PORT MAP (
+          DatoEntrada=>DatoEntrada,
+          clk => clk,
+          reset => reset,
+          segmento => segmento,
+          anodo=>anodo
+        );
+
+DatoEntrada <= "00011011010110";
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+S_reset:PROCESS
+BEGIN
+reset<= '1';
+WAIT FOR 6ns;
+reset<= '0';
+wait;
+end process;
+
+
+end Behavioral;
