fbd7e689a77d3a3593c7ddb7d9d78896c3872128
César Augusto Romero Molano <ceroma175@gmail.com>
2023-08-28 15:48:44 -0500

    TesEliminaReboteTemporizado

---
+++ MaquinaEstados/VerificaTres/VerificaUno.srcs/sources_1/new/SuperiorVerifica.vhd
@@ -0,0 +1,44 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity SuperiorVerifica is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           ce : in  STD_LOGIC;
+           segmento : out  STD_LOGIC_VECTOR (6 downto 0);
+           anodo : out  STD_LOGIC_VECTOR (3 downto 0));
+end SuperiorVerifica;
+
+architecture Behavioral of SuperiorVerifica is
+
+component SupReboteTemporizado is
+    Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           Boton : in STD_LOGIC;
+           Boton_out : out STD_LOGIC);
+end component;
+
+component SuperiorBinarioDecimal is
+    Port ( DatoEntrada : in  STD_LOGIC_VECTOR (13 downto 0);
+           clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           segmento : out  STD_LOGIC_VECTOR (6 downto 0);
+           anodo : out  STD_LOGIC_VECTOR (3 downto 0));
+end component;
+
+component cuenta2 is
+    Port ( clk : in STD_LOGIC;
+           clear : in STD_LOGIC;
+           ce : in STD_LOGIC;
+           salidacuenta : out STD_LOGIC_VECTOR (13 downto 0));
+end component;
+
+signal sg0:STD_LOGIC;
+signal sg1:STD_LOGIC_VECTOR (13 downto 0);
+
+begin
+
+u0:SupReboteTemporizado Port map(clk=>clk,reset=>reset,Boton=>ce,Boton_out=>sg0);
+u1: cuenta2 Port map( clk=>clk,clear=>reset,ce=>sg0,salidacuenta=>sg1);
+u2: SuperiorBinarioDecimal Port map(DatoEntrada=>sg1,clk=>clk,reset=>reset,segmento=>segmento,anodo=>anodo); 
+end Behavioral;
---
+++ MaquinaEstados/VerificaTres/VerificaUno.srcs/sources_1/new/cuenta1.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_unsigned.all;
+
+entity cuenta1 is
+    Port ( clk : in STD_LOGIC;
+           clear : in STD_LOGIC;
+           FoutCuenta : out STD_LOGIC_VECTOR (2 downto 0));
+end cuenta1;
+
+architecture Behavioral of cuenta1 is
+signal cuenta:STD_LOGIC_VECTOR (2 downto 0);
+
+begin
+	process(clk,clear)
+		begin
+		if clear = '1' then
+		cuenta <= (others =>'0');
+		elsif clk' event and clk ='1' then
+		if cuenta = "100" then
+			cuenta <="001"; --(others=>'0') ;
+		else
+			cuenta <= (cuenta + 1);
+		end if;
+		end if;
+	end process;
+	FoutCuenta<=cuenta;
+end Behavioral;
