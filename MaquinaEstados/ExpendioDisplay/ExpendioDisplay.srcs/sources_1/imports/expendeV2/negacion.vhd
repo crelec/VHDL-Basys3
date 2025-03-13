55ea0a71f5507955c7a4427ca7228c62c4588e93
César Augusto Romero Molano <111470363+crelec@users.noreply.github.com>
2025-03-13 04:42:47 -0500

    AdicionoAplicación
    Adiciono aplicación Maquina de estados expendedora

---
+++ MaquinaEstados/ExpendioDisplay/ExpendioDisplay.srcs/sources_1/imports/expendeV2/negacion.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity negacion is
+    Port ( btn : in  STD_LOGIC_VECTOR (1 downto 0);
+           anodo : out  STD_LOGIC_VECTOR (3 downto 0));
+end negacion;
+
+architecture Behavioral of negacion is
+
+begin
+process (btn)
+begin
+	case btn is
+		when "00" =>anodo<="1110";
+		when "01" =>anodo<="1101";
+		when "10" =>anodo<="1011";
+		when others =>anodo<="1111";
+	end case;
+end process;end Behavioral;
